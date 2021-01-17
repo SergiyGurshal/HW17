05769ba44f82180e63b2e9b8903ed8f658380555
Sergiy <41513150+SergiyGurshal@users.noreply.github.com>
2021-01-17 18:49:53 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Timer from './Timer'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Timer
+        time="10"
+        autostart={true}
+        onTick={(time) => console.log('Залишилось часу: ' + time)}
+        step="2"
+        onTimeEnd={() => console.log('Час вийшов!')}
+        onTimeStart={(timeLeft) => console.log('Таймер запущено!')}
+        onTimePause={(timeLeft) => console.log('Таймер на паузі!')}
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Timer.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react'
+
+export default class Timer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { time: {}, seconds: this.props.time, width: 100 }
+    this.timer = 0
+    this.startTimer = this.startTimer.bind(this)
+    this.countDown = this.countDown.bind(this)
+    this.pauseTimer = this.pauseTimer.bind(this)
+  }
+
+  parsTime(secs) {
+    const hours = Math.floor(secs / (60 * 60))
+
+    const divisor_for_minutes = secs % (60 * 60)
+    const minutes = Math.floor(divisor_for_minutes / 60)
+
+    const divisor_for_seconds = divisor_for_minutes % 60
+    const seconds = Math.ceil(divisor_for_seconds)
+
+    const obj = {
+      h: hours,
+      m: minutes,
+      s: seconds,
+    }
+    return obj
+  }
+
+  startTimer() {
+    if (this.state.seconds > 0) {
+      this.timer = setInterval(this.countDown, this.props.step * 1000)
+      this.props.onTimeStart(this.state.seconds)
+    }
+  }
+
+  componentDidMount() {
+    let timeLeftVar = this.parsTime(this.state.seconds)
+    this.setState({ time: timeLeftVar })
+
+    if (this.props.autostart) {
+      this.startTimer()
+    }
+  }
+
+  countDown() {
+    let seconds = this.state.seconds - this.props.step
+    this.setState({
+      time: this.parsTime(seconds),
+      seconds: seconds,
+    })
+
+    this.setState({ width: this.state.width - (100 / this.props.time) * this.props.step })
+    this.props.onTick(this.state.seconds)
+
+    if (seconds === 0) {
+      this.props.onTimeEnd()
+      clearInterval(this.timer)
+    }
+  }
+
+  pauseTimer() {
+    this.props.onTimePause(this.state.seconds)
+    clearInterval(this.timer)
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          {this.state.time.h}:{this.state.time.m}:{this.state.time.s}
+        </div>
+        <div className="loadBar" style={{ width: `${this.state.width}%` }}></div>
+        <button onClick={this.startTimer}>Start</button>
+        <button onClick={this.pauseTimer}>Pause</button>
+      </div>
+    )
+  }
+}
